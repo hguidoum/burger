3af7b2cb806925534d0ac9a3648c977ae55c7321
Hakim Guidoum <gdmhkm05@gmail.com>
2019-10-10 12:13:19 -0400

    updated connection and package json

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "12345678",
-  database: "cat_db"
-});
+var connection;
 
-// Make connection.
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "12345678",
+    database: "burgers_db"
+  });
+}
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,5 +22,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
